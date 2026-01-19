f4ebdb43c481c585f72f5fdb433072bdd8ed36b1
zhengyb <zhengyb@inhand.com.cn>
2026-01-19 14:06:55 -0500

    rm new sim

--- tools/carla-sim-new/__init__.py
+++
@@ -1,6 +0,0 @@
-"""CARLA 0.9.15 simulator bridge for openpilot (Python 3.12).
-
-Place-holder module file to enable absolute imports under
-`openpilot.tools.carla_sim_new`.
-"""
-
--- tools/carla-sim-new/bridge/__init__.py
+++
@@ -1,2 +0,0 @@
-# namespace for carla-sim-new bridge
-
