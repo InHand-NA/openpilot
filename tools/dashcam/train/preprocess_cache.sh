7dae3e4b2d51804ab8f533eec8d968f490adc5f7
zhengyb <zhengyb@inhand.com.cn>
2026-03-06 16:39:53 -0500

    add cache

---
+++ tools/dashcam/train/preprocess_cache.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+ANNOTATED_DIR=$1
+for H in H1 H2 H3 H4 H5 H6; do
+    Hk_DIR="${ANNOTATED_DIR}/${H}"
+    echo "Checking for cached data for ${H} in ${Hk_DIR}..."
+    if [ -d "${Hk_DIR}" ]; then
+        echo "Preprocessing cached data for ${H}..."
+        python tools/dashcam/train/preprocess_cache.py \
+            ${Hk_DIR}
+    fi
+done
